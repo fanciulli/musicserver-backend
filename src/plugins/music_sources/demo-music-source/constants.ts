b6cfaa962e9178e04a15a4e23d7bfc59feaa0338
Massimiliano Fanciulli <fanciulli@gmail.com>
2026-03-10 20:19:57 +0100

    Removing demo plugin.

--- src/plugins/music_sources/demo-music-source/constants.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Created on Wed Mar 04 2026
- *
- * Author: Massimiliano Fanciulli
- *
- * GitHub: https://github.com/fanciulli
- */
-export const PLUGIN_ID = "demo-music-source";
-export const PLUGIN_NAME = "Demo Music Source";
