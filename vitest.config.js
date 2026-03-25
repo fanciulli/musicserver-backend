e2354623b23ec8822ddca043a7df6d6f37bb15be
Massimiliano Fanciulli <fanciulli@gmail.com>
2026-03-25 17:57:33 +0100

    Excluded types from Unit Testing.

--- vitest.config.js
+++ vitest.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
     coverage: {
       provider: "v8",
       include: ["src/**/*.ts"],
+      exclude: ["src/types/**"],
       reporter: ["text", "html"],
       thresholds: {
         statements: 1,
